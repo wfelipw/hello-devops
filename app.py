e1724fd0160da181e905eb98b106c280c67de01d
Felipe <felipewja56560@gmail.com>
2026-05-05 20:06:15 -0300

    atualizamensagem

--- app.py
+++ app.py
@@ -2,6 +2,6 @@ from flask import Flask
 app = Flask(__name__)
 @app.route('/')
 def hello():
- return 'Hello, Turma DevOps 2025!'
+ return 'Hello, Turma DevOps 2025.!'
 if __name__ == '__main__':
  app.run(host='0.0.0.0', port=10000)
